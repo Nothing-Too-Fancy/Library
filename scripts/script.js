be605d41a5148e8f389b6788cc271fe55f7bd456
Ed Finlaw <edrfinlaw@gmail.com>
2021-04-30 13:40:57 -0700

    created a demo book object and added information from it to the DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,49 @@
 let myLibrary = [];
 
 //DOM Objects
+const _table = document.querySelector('.books');
+const _tbody = document.querySelector('.lib-body');
+const _editDelete = document.querySelector('.edit-delete');
+const _new = document.querySelector('.new');
 
-
-function Book() {
-   title: 'Unknown';
-   author: 'Unknown';
-   totalPages: 0;
-   pagesRead: 0;
-   read: false;
+function Book(title, author, total, readPages, read) {
+   this.title = title;
+   this.author = author;
+   this.totalPages = total;
+   this.pagesRead = readPages;
+   this.read = read;
 }
 
+let newBook = new Book(
+   title = 'Harry Potter',
+   author = 'JK Rowling',
+   totalPages = 300,
+   pagesRead = 35,
+   read = false
+   );
+   
 function addBookToLibrary(book) {
+   let tr = document.createElement('tr');
+   let tdTitle = document.createElement('td');
+   let tdAuthor = document.createElement('td');
+   let tdPages = document.createElement('td');
+   let tdRead = document.createElement('td');
 
+   tdTitle.textContent = book.title;
+   tdAuthor.textContent = book.author;
+   tdPages.textContent = `${book.pagesRead}/${book.totalPages}`;
+   tdRead.textContent = book.read ? 'Read' : 'Not Read';
+
+   tr.appendChild(tdTitle);
+   tr.appendChild(tdAuthor);
+   tr.appendChild(tdPages);
+   tr.appendChild(tdRead);
+   tr.appendChild(_editDelete.cloneNode(true));
+   tr.appendChild(_new);
+
+   _tbody.appendChild(tr);
+
+   myLibrary.push(book);
 }
+
+addBookToLibrary(newBook);

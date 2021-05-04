ca44811e99db262bbd551c0f2d47418e313746b4
Ed Finlaw <edrfinlaw@gmail.com>
2021-05-04 16:06:20 -0700

    added form validation and added event handler to the form submit as well as rendering the new book in the table

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,21 @@ const _author = document.querySelector('.author');
 const _readPages = document.querySelector('.pages-read');
 const _totalPages = document.querySelector('.total-pages');
 const _status = document.querySelector('.status');
+const _submit = document.querySelector('.submit').addEventListener('click', 
+   () => {
+      let newBook = new Book(_title.value, _author.value, _totalPages.value, 
+         _readPages.value, _status.value);
+
+      addBookToLibrary(newBook);
+});
+const _cancel = document.querySelector('.cancel').addEventListener('click',
+   () => {
+      if (myLibrary.length != 0)
+      {
+         _form.remove();
+      }
+
+});
 const _editDelete =
 `<td class='edit-delete'>
    <button class='edit'>Edit</button>
@@ -23,31 +38,62 @@ const _new =
 function Book(title, author, total, readPages, status) {
    this.title = title;
    this.author = author;
-   this.totalPages = total;
-   this.pagesRead = readPages;
+   this.totalPages = parseInt(total);
+   this.pagesRead = parseInt(readPages);
    this.status = status;
 }
    
-function addBookToLibrary(book) {
-   let tr = document.createElement('tr');
-   let tdTitle = document.createElement('td');
-   let tdAuthor = document.createElement('td');
-   let tdPages = document.createElement('td');
-   let tdStatus = document.createElement('td');
+function addBookToLibrary(book)
+{
+   if (book.title      === '' || 
+       book.author     === '' ||
+       isNaN(book.totalPages) ||
+       isNaN(book.pagesRead))
+   {
 
-   tdTitle.textContent = book.title;
-   tdAuthor.textContent = book.author;
-   tdPages.textContent = `${book.pagesRead}/${book.totalPages}`;
-   tdStatus.textContent = book.status ? 'Read' : 'Not Read';
-
-   tr.appendChild(tdTitle);
-   tr.appendChild(tdAuthor);
-   tr.appendChild(tdPages);
-   tr.appendChild(tdStatus);
-   tr.appendChild(_editDelete.cloneNode(true));
-   tr.appendChild(_new);
-
-   _tbody.appendChild(tr);
+      alert("Please fill out all fields.");
+      return;
+   }
+   else if (typeof book.totalPages != 'number' ||
+            book.totalPages < 0 ||
+            typeof book.pagesRead  != 'number' ||
+            book.pagesRead < 0)
+   {
+      alert("Please enter a positive number for page numbers.");
+      return;
+   }
+   else if (book.totalPages < book.pagesRead)
+   {
+      alert("Total pages cannot be less than pages read.");
+      return;
+   }
 
    myLibrary.push(book);
+   render();
+}
+
+function render()
+{
+   _tbody.innerHTML = '';
+   myLibrary.forEach((book) => {
+      let tr = document.createElement('tr');
+      let tdTitle = document.createElement('td');
+      let tdAuthor = document.createElement('td');
+      let tdPages = document.createElement('td');
+      let tdStatus = document.createElement('td');
+
+      tdTitle.textContent = book.title;
+      tdAuthor.textContent = book.author;
+      tdPages.textContent = `${book.pagesRead}/${book.totalPages}`;
+      tdStatus.textContent = book.status ? 'Read' : 'Not Read';
+
+      tr.appendChild(tdTitle);
+      tr.appendChild(tdAuthor);
+      tr.appendChild(tdPages);
+      tr.appendChild(tdStatus);
+      tr.insertAdjacentHTML('beforeend', _editDelete);
+      tr.insertAdjacentHTML('beforeend', _new);
+
+      _tbody.appendChild(tr);
+   })
 }

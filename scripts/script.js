cdd806d65f4ace6a798fe54f9070f54d875cafb1
Ed Finlaw <edrfinlaw@gmail.com>
2021-05-06 14:59:02 -0700

    added functionality to edit book information for an existing book

--- scripts/script.js
+++ scripts/script.js
@@ -11,28 +11,70 @@ const _totalPages = document.querySelector('.total-pages');
 const _status = document.querySelector('.status');
 const _submit = document.querySelector('.submit').addEventListener('click', 
    () => {
+      if (_form.hasAttribute('data-index')) {
+         let book = myLibrary[parseInt(_form.dataset.index)];
+         book.title = _title.value;
+         book.author = _author.value;
+         book.readPages = parseInt(_readPages.value);
+         book.totalPages = parseInt(_totalPages.value);
+         book.status = _status.value;
+
+         myLibrary[parseInt(_form.dataset.index)] = book;
+         render();
+      }
+      else {
       let newBook = new Book(_title.value, _author.value, _totalPages.value, 
          _readPages.value, _status.value);
 
       addBookToLibrary(newBook);
+      }
 });
 const _cancel = document.querySelector('.cancel').addEventListener('click',
    () => {
       if (myLibrary.length != 0)
       {
          _form.remove();
+         render();
       }
 
 });
-const _editDelete =
-`<td class='edit-delete'>
-   <button class='edit'>Edit</button>
-   <button class='delete'>Delete</button>
-</td>`;
-const _new =
-`<td class='new'>
-   <button class='newbook'>+</button>
-</td>`;
+
+function buildEditDelete() {
+   let td = document.createElement('td');
+   td.classList.add('edit-delete');
+   let editButton = document.createElement('button');
+   editButton.classList.add('edit');
+   editButton.textContent = 'Edit';
+   let deleteButton = document.createElement('button');
+   deleteButton.classList.add('delete');
+   deleteButton.textContent = 'Delete';
+
+   td.appendChild(editButton);
+   td.appendChild(deleteButton);
+
+   return td;
+}
+const _editDelete = buildEditDelete();
+
+function buildNew() {
+   let td = document.createElement('td');
+   td.classList.add('new');
+   let editButton = document.createElement('button');
+   editButton.classList.add('newbook');
+   editButton.textContent = '+';
+
+   td.appendChild(editButton);
+   return td;
+}
+const _new = buildNew();
+_new.addEventListener('click', () => {
+   _title.value = '';
+   _author.value = '';
+   _readPages.value = '';
+   _totalPages.value = '';
+   _status.value = 'Not Read';
+   _tbody.appendChild(_form);
+})
 
 //Book object constructor
 function Book(title, author, total, readPages, status) {
@@ -76,6 +118,7 @@ function render()
 {
    _tbody.innerHTML = '';
    myLibrary.forEach((book) => {
+      let index = myLibrary.indexOf(book);
       let tr = document.createElement('tr');
       let tdTitle = document.createElement('td');
       let tdAuthor = document.createElement('td');
@@ -85,14 +128,25 @@ function render()
       tdTitle.textContent = book.title;
       tdAuthor.textContent = book.author;
       tdPages.textContent = `${book.pagesRead}/${book.totalPages}`;
-      tdStatus.textContent = book.status ? 'Read' : 'Not Read';
+      tdStatus.textContent = book.status;
 
       tr.appendChild(tdTitle);
       tr.appendChild(tdAuthor);
       tr.appendChild(tdPages);
       tr.appendChild(tdStatus);
-      tr.insertAdjacentHTML('beforeend', _editDelete);
-      tr.insertAdjacentHTML('beforeend', _new);
+      let editDeleteCopy = _editDelete.cloneNode(true);
+      let editBtn = editDeleteCopy.querySelector('.edit');
+      editBtn.setAttribute('data-index', `${index}`);
+      editBtn.addEventListener('click', () => {
+         _form.setAttribute('data-index', editBtn.dataset.index);
+         _tbody.replaceChild(_form, editBtn.parentElement.parentElement);
+         });
+
+      let deleteBtn = editDeleteCopy.querySelector('.delete');
+      deleteBtn.setAttribute('data-index', `${index}`);
+
+      tr.appendChild(editDeleteCopy);
+      tr.appendChild(_new);
 
       _tbody.appendChild(tr);
    })
